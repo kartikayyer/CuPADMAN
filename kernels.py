cf71da6a98512493e10831f9a3f42628f5d0393b
Kartik Ayyer <kartik.ayyer@desy.de>
2019-11-27 01:26:25 +0100

    kernels: Converted slice_gen and slice_merge for 3D models

--- kernels.py
+++ kernels.py
@@ -1,44 +1,87 @@
 import cupy as cp
 
 slice_gen = cp.RawKernel(r'''
+    extern "C" __device__
+    void gen_rot(const double quaternion[4], double rot[3][3]) {
+        double q0, q1, q2, q3, q01, q02, q03, q11, q12, q13, q22, q23, q33 ;
+        
+        q0 = quaternion[0] ;
+        q1 = quaternion[1] ;
+        q2 = quaternion[2] ;
+        q3 = quaternion[3] ;
+        
+        q01 = q0*q1 ;
+        q02 = q0*q2 ;
+        q03 = q0*q3 ;
+        q11 = q1*q1 ;
+        q12 = q1*q2 ;
+        q13 = q1*q3 ;
+        q22 = q2*q2 ;
+        q23 = q2*q3 ;
+        q33 = q3*q3 ;
+        
+        rot[0][0] = (1. - 2.*(q22 + q33)) ;
+        rot[0][1] = 2.*(q12 + q03) ;
+        rot[0][2] = 2.*(q13 - q02) ;
+        rot[1][0] = 2.*(q12 - q03) ;
+        rot[1][1] = (1. - 2.*(q11 + q33)) ;
+        rot[1][2] = 2.*(q01 + q23) ;
+        rot[2][0] = 2.*(q02 + q13) ;
+        rot[2][1] = 2.*(q23 - q01) ;
+        rot[2][2] = (1. - 2.*(q11 + q22)) ;
+    }
+
     extern "C" __global__
     void slice_gen(const double *model,
-                   const double angle,
+                   const double *quat,
+                   const double *qvals,
                    const double scale,
+                   const long long num_pix,
                    const long long size,
-                   const double *bg,
                    const long long log_flag,
                    double *view) {
-        int x = blockIdx.x * blockDim.x + threadIdx.x ;
-        int y = blockIdx.y * blockDim.y + threadIdx.y ;
-        if (x > size - 1 || y > size - 1)
-            return ;
-        int t = x*size + y ;
-        if (log_flag)
-            view[t] = -1000. ;
-        else
-            view[t] = 0. ;
-
-        int cen = size / 2 ;
-        double ac = cos(angle), as = sin(angle) ;
-        double tx = (x - cen) * ac - (y - cen) * as + cen ;
-        double ty = (x - cen) * as + (y - cen) * ac + cen ;
-        int ix = __double2int_rd(tx), iy = __double2int_rd(ty) ;
-        if (ix < 0 || ix > size - 2 || iy < 0 || iy > size - 2)
+        int t = blockIdx.x * blockDim.x + threadIdx.x ;
+        int i, j, cen = size/2 ;
+        double rot_pix[3], rot[3][3] ;
+        if (t >= num_pix)
             return ;
 
-        double fx = tx - ix, fy = ty - iy ;
-        double cx = 1. - fx, cy = 1. - fy ;
+        gen_rot(quat, rot) ;
+        
+        for (i = 0 ; i < 3 ; ++i) {
+            rot_pix[i] = 0. ;
+            for (j = 0 ; j < 3 ; ++j) 
+                rot_pix[i] += rot[i][j] * qvals[t*3 + j] ;
+            rot_pix[i] += cen ;
+        }
 
-        view[t] = model[ix*size + iy]*cx*cy + 
-                  model[(ix+1)*size + iy]*fx*cy +
-                  model[ix*size + (iy+1)]*cx*fy + 
-                  model[(ix+1)*size + (iy+1)]*fx*fy ;
+        int ix = __double2int_rd(rot_pix[0]) ;
+        int iy = __double2int_rd(rot_pix[1]) ;
+        int iz = __double2int_rd(rot_pix[2]) ;
+        if (ix < 0 || ix > size - 2 || iy < 0 || iy > size - 2) {
+            if (log_flag)
+                view[t] = -1.0e20 ;
+            else
+                view[t] = 0. ;
+        
+            return ;
+        }
+
+        double fx = rot_pix[0] - ix, fy = rot_pix[1] - iy, fz = rot_pix[2] - iz ;
+        double cx = 1. - fx, cy = 1. - fy, cz = 1. - fz ;
+
+        view[t] = cx*cy*cz*model[ix*size*size + iy*size + iz] + 
+                  cx*cy*fz*model[ix*size*size + iy*size + iz+1] +
+                  cx*fy*cz*model[ix*size*size + (iy+1)*size + iz] + 
+                  cx*fy*fz*model[ix*size*size + (iy+1)*size + iz+1] +
+                  fx*cy*cz*model[(ix+1)*size*size + iy*size + iz] + 
+                  fx*cy*fz*model[(ix+1)*size*size + iy*size + iz+1] +
+                  fx*fy*cz*model[(ix+1)*size*size + (iy+1)*size + iz] + 
+                  fx*fy*fz*model[(ix+1)*size*size + (iy+1)*size + iz+1] ;
         view[t] *= scale ;
-        view[t] += bg[t] ;
         if (log_flag) {
             if (view[t] < 1.e-20)
-                view[t] = -1000. ;
+                view[t] = -1.0e20 ;
             else
                 view[t] = log(view[t]) ;
         }
@@ -46,39 +89,88 @@ slice_gen = cp.RawKernel(r'''
     ''', 'slice_gen')
 
 slice_merge = cp.RawKernel(r'''
+    extern "C" __device__
+    void gen_rot(const double quaternion[4], double rot[3][3]) {
+        double q0, q1, q2, q3, q01, q02, q03, q11, q12, q13, q22, q23, q33 ;
+        
+        q0 = quaternion[0] ;
+        q1 = quaternion[1] ;
+        q2 = quaternion[2] ;
+        q3 = quaternion[3] ;
+        
+        q01 = q0*q1 ;
+        q02 = q0*q2 ;
+        q03 = q0*q3 ;
+        q11 = q1*q1 ;
+        q12 = q1*q2 ;
+        q13 = q1*q3 ;
+        q22 = q2*q2 ;
+        q23 = q2*q3 ;
+        q33 = q3*q3 ;
+        
+        rot[0][0] = (1. - 2.*(q22 + q33)) ;
+        rot[0][1] = 2.*(q12 + q03) ;
+        rot[0][2] = 2.*(q13 - q02) ;
+        rot[1][0] = 2.*(q12 - q03) ;
+        rot[1][1] = (1. - 2.*(q11 + q33)) ;
+        rot[1][2] = 2.*(q01 + q23) ;
+        rot[2][0] = 2.*(q02 + q13) ;
+        rot[2][1] = 2.*(q23 - q01) ;
+        rot[2][2] = (1. - 2.*(q11 + q22)) ;
+    }
+
     extern "C" __global__
     void slice_merge(const double *view,
-                     const double angle,
+                     const double *quat,
+                     const double *qvals,
+                     const long long num_pix,
                      const long long size,
                      double *model,
                      double *mweights) {
-        int x = blockIdx.x * blockDim.x + threadIdx.x ;
-        int y = blockIdx.y * blockDim.y + threadIdx.y ;
-        if (x > size - 1 || y > size - 1)
+        int t = blockIdx.x * blockDim.x + threadIdx.x ;
+        if (t >= num_pix)
             return ;
-        int t = x*size + y ;
 
-        int cen = size / 2 ;
-        double ac = cos(angle), as = sin(angle) ;
-        double tx = (x - cen) * ac - (y - cen) * as + cen ;
-        double ty = (x - cen) * as + (y - cen) * ac + cen ;
-        int ix = __double2int_rd(tx), iy = __double2int_rd(ty) ;
+        int i, j, cen = size/2 ;
+        double rot_pix[3], rot[3][3] ;
+        if (t >= num_pix)
+            return ;
+
+        gen_rot(quat, rot) ;
+        
+        for (i = 0 ; i < 3 ; ++i) {
+            rot_pix[i] = 0. ;
+            for (j = 0 ; j < 3 ; ++j) 
+                rot_pix[i] += rot[i][j] * qvals[t*3 + j] ;
+            rot_pix[i] += cen ;
+        }
+
+        int ix = __double2int_rd(rot_pix[0]) ;
+        int iy = __double2int_rd(rot_pix[1]) ;
+        int iz = __double2int_rd(rot_pix[2]) ;
         if (ix < 0 || ix > size - 2 || iy < 0 || iy > size - 2)
             return ;
-        double fx = tx - ix, fy = ty - iy ;
-        double cx = 1. - fx, cy = 1. - fy ;
 
-        atomicAdd(&model[ix*size + iy], view[t]*cx*cy) ;
-        atomicAdd(&mweights[ix*size + iy], cx*cy) ;
+        double fx = rot_pix[0] - ix, fy = rot_pix[1] - iy, fz = rot_pix[2] - iz ;
+        double cx = 1. - fx, cy = 1. - fy, cz = 1. - fz ;
 
-        atomicAdd(&model[(ix+1)*size + iy], view[t]*fx*cy) ;
-        atomicAdd(&mweights[(ix+1)*size + iy], fx*cy) ;
+        atomicAdd(&model[ix*size*size + iy*size + iz], view[t]*cx*cy*cz) ;
+        atomicAdd(&model[ix*size*size + iy*size + iz+1], view[t]*cx*cy*fz) ;
+        atomicAdd(&model[ix*size*size + (iy+1)*size + iz], view[t]*cx*fy*cz) ;
+        atomicAdd(&model[ix*size*size + (iy+1)*size + iz+1], view[t]*cx*fy*fz) ;
+        atomicAdd(&model[(ix+1)*size*size + iy*size + iz], view[t]*fx*cy*cz) ;
+        atomicAdd(&model[(ix+1)*size*size + iy*size + iz+1], view[t]*fx*cy*fz) ;
+        atomicAdd(&model[(ix+1)*size*size + (iy+1)*size + iz], view[t]*fx*fy*cz) ;
+        atomicAdd(&model[(ix+1)*size*size + (iy+1)*size + iz+1], view[t]*fx*fy*fz) ;
 
-        atomicAdd(&model[ix*size + (iy+1)], view[t]*cx*fy) ;
-        atomicAdd(&mweights[ix*size + (iy+1)], cx*fy) ;
-
-        atomicAdd(&model[(ix+1)*size + (iy+1)], view[t]*fx*fy) ;
-        atomicAdd(&mweights[(ix+1)*size + (iy+1)], fx*fy) ;
+        atomicAdd(&mweights[ix*size*size + iy*size + iz], cx*cy*cz) ;
+        atomicAdd(&mweights[ix*size*size + iy*size + iz+1], cx*cy*fz) ;
+        atomicAdd(&mweights[ix*size*size + (iy+1)*size + iz], cx*fy*cz) ;
+        atomicAdd(&mweights[ix*size*size + (iy+1)*size + iz+1], cx*fy*fz) ;
+        atomicAdd(&mweights[(ix+1)*size*size + iy*size + iz], fx*cy*cz) ;
+        atomicAdd(&mweights[(ix+1)*size*size + iy*size + iz+1], fx*cy*fz) ;
+        atomicAdd(&mweights[(ix+1)*size*size + (iy+1)*size + iz], fx*fy*cz) ;
+        atomicAdd(&mweights[(ix+1)*size*size + (iy+1)*size + iz+1], fx*fy*fz) ;
     }
     ''', 'slice_merge')
 
