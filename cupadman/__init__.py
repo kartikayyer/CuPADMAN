a2feaddaa6afd4902d119501de703edc906ee1f2
Kartik Ayyer <kartik.ayyer@desy.de>
2019-11-29 11:10:29 +0100

    __init__: Exposed EMC and DataGenerator classes

--- cupadman/__init__.py
+++ cupadman/__init__.py
@@ -1,3 +1,5 @@
 from .detector import Detector, CDetector
 from .emcfile import EMCReader, EMCWriter, CDataset
 from .quaternion import Quaternion
+from .emc import EMC
+from .make_data import DataGenerator
